febf03d48a6a67f299bc5a86457f8e47d16d8299
Yaroslav Savin <molniyar11@gmail.com>
2024-02-02 15:37:16 +0300

    9 Palindrome number

---
+++ src/Task9_palindromeNumber/PalindrimeNumber.java
@@ -0,0 +1,22 @@
+package Task9_palindromeNumber;
+
+public class PalindrimeNumber {
+    public static void main(String[] args) {
+        int test  = 2345;
+        int test1 = 121;
+        int test2 = -191;
+        int test3 = -200;
+        int test4 = 12766721;
+
+        System.out.println(new PalindrimeNumber().isPalindrome(test2));
+    }
+    public boolean isPalindrome(int x) {
+        String xLine = x + "";
+        String eniLx = "";//xLine, только наоборот(записывается в цикле ниже)
+        for (int i = xLine.length() - 1; i >= 0; i--){
+            eniLx = eniLx + xLine.charAt(i);
+        }
+        boolean result = xLine.equals(eniLx);
+        return result;
+    }
+}
