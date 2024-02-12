696dc022ac1a48673c313087fd3516b430885dc2
Yaroslav Savin <molniyar11@gmail.com>
2024-02-12 14:27:53 +0300

    2769 Find the maximum achieveable number

---
+++ src/Task2769_FindTheMaximumAchieveableNumber/FindTheMaximumAchieveableNumber.java
@@ -0,0 +1,16 @@
+package Task2769_FindTheMaximumAchieveableNumber;
+
+public class FindTheMaximumAchieveableNumber {
+    public static void main(String[] args) {
+        int num  = 4;
+        int num1 = 3;
+        int t  = 1;
+        int t1 = 2;
+
+        System.out.println(new FindTheMaximumAchieveableNumber().theMaximumAchievableX(num1, t1));
+    }
+    public int theMaximumAchievableX(int num, int t) {
+        int x = num + t + t;
+        return x;
+    }
+}
