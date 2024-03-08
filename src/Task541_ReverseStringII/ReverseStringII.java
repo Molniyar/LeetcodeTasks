94ad80c1a1734ed38b65f923e34c802766fae51e
Yaroslav Savin <molniyar11@gmail.com>
2024-03-08 22:26:12 +0400

    541 Reverse string II

---
+++ src/Task541_ReverseStringII/ReverseStringII.java
@@ -0,0 +1,23 @@
+package Task541_ReverseStringII;
+
+import java.util.Arrays;
+
+public class ReverseStringII {
+    public static void main(String[] args) {
+        System.out.println(new ReverseStringII().reverseStr("a", 2));
+    }
+    public String reverseStr(String s, int k) {
+        char[] sArray = s.toCharArray();
+        for (int i = 0; i < s.length(); i += k * 2){
+            StringBuilder sb = new StringBuilder();
+            for (int j = i; j < k + i && j < s.length(); j++){
+                sb.append(sArray[j]);
+            }
+            sb.reverse();
+            for (int j = 0; j < sb.length(); j++){
+                sArray[j + i] = sb.charAt(j);
+            }
+        }
+        return new String(sArray);
+    }
+}
