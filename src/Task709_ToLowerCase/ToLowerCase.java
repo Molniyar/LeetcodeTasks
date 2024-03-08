ca14d6b332e82f8fe3f413b4d757e66471a9c6f7
Yaroslav Savin <molniyar11@gmail.com>
2024-03-08 20:59:46 +0400

    709 To lower case

---
+++ src/Task709_ToLowerCase/ToLowerCase.java
@@ -0,0 +1,14 @@
+package Task709_ToLowerCase;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println("АРБУЗ".toLowerCase());
+    }
+    public String toLowerCase(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++){
+            //switch ()
+        }
+        return "";
+    }
+}
