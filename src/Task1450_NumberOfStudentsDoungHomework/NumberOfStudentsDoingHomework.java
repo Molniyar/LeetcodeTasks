105d2272dafdc4943f03159059bcf8e279a1ce6d
Yaroslav Savin <molniyar11@gmail.com>
2024-03-13 20:14:42 +0400

    1450 Number of students doing homework

---
+++ src/Task1450_NumberOfStudentsDoungHomework/NumberOfStudentsDoingHomework.java
@@ -0,0 +1,18 @@
+package Task1450_NumberOfStudentsDoungHomework;
+
+public class NumberOfStudentsDoingHomework {
+    public static void main(String[] args) {
+        int[] startTiime = {1,2,3};
+        int[] endTime = {3,2,7};
+        System.out.println(new NumberOfStudentsDoingHomework().busyStudent(startTiime, endTime, 3));
+    }
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int result = 0;
+        for (int i = 0; i < startTime.length; i++){
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]){
+                result++;
+            }
+        }
+        return result;
+    }
+}
