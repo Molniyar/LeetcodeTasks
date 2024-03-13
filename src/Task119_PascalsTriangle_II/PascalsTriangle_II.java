99b1913c57de5269e200853321ad6a98d2dfd2a1
Yaroslav Savin <molniyar11@gmail.com>
2024-03-13 20:15:42 +0400

    119 Pascals triangle II

--- src/Task119_PascalsTriangle_II/PascalsTriangle_II.java
+++ src/Task119_PascalsTriangle_II/PascalsTriangle_II.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class PascalsTriangle_II {
     public static void main(String[] args) {
-        System.out.println(new PascalsTriangle_II().getRow(3));
+        System.out.println(new PascalsTriangle_II().getRow(5));
     }
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new ArrayList<>(rowIndex + 1);
@@ -13,12 +13,10 @@ public class PascalsTriangle_II {
             result.add(0);
         }
         for (int i = 0; i <= rowIndex; i++){
-            for (int j = i; j >= 0; j--){
-                if (j == 0 || j == i){
-                    result.set(j, 1);
-                }
-                else result.set(j, (result.get(j) + result.get(j - 1)));
+            for (int j = i - 1; j > 0; j--){
+                result.set(j, (result.get(j) + result.get(j - 1)));
             }
+            result.set(i, 1);
         }
         return result;
     }
