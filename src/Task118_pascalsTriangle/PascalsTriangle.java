2279df77b7e4c8bbb038015f94defaa73bf5170e
Yaroslav Savin <molniyar11@gmail.com>
2024-03-13 20:15:16 +0400

    118 Pascals triangle упрощен код

--- src/Task118_pascalsTriangle/PascalsTriangle.java
+++ src/Task118_pascalsTriangle/PascalsTriangle.java
@@ -8,29 +8,22 @@ public class PascalsTriangle {
         System.out.println(new PascalsTriangle().generate(7));
     }
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>(numRows);
+        List<Integer> previous = new ArrayList<>(1);
+        previous.add(1);
+        result.add(previous);
+        for (int i = 1; i < numRows; i++){
+            List<Integer> current = new ArrayList<>(i + 1);
+            current.add(1);
+            for (int j = 1; j < i; j++) {
+                int a = previous.get(j - 1);
+                int b = previous.get(j);
+                current.add(a + b);
+            }
+            current.add(1);
 
-        for (int i = 0; i < numRows; i++){
-            result.add(new ArrayList<>());
-            for (int j2 = 0; j2 <= i; j2++){
-                result.get(i).add(1);
-            }
-            if (i > 1) {
-                for (int j = 0; j < i - 1; j++) {
-                    int minusI = i - 1;
-                    if (minusI >= result.size()){
-                        minusI = result.size() - 1;
-                    }
-                    //result.get(i + 1).add(j, )
-                    int plusJ = j + 1;
-                    if (plusJ >= result.get(minusI).size()){
-                        plusJ = result.get(minusI).size() - 1;
-                    }
-                    int a = result.get(minusI).get(j);
-                    int b = result.get(minusI).get(plusJ);
-                    result.get(i).set(j + 1,a + b);
-                }
-            }
+            result.add(current);
+            previous = current;
         }
         return result;
     }
