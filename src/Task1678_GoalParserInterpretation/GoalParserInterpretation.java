73a301cdbc750700c29f8476e16c616b813597b4
Yaroslav Savin <molniyar11@gmail.com>
2024-03-16 17:34:52 +0400

    1678 Goal parser interpretation

--- src/Task1678_GoalParserInterpretation/GoalParserInterpretation.java
+++ src/Task1678_GoalParserInterpretation/GoalParserInterpretation.java
@@ -9,27 +9,14 @@ public class GoalParserInterpretation {
         System.out.println(new GoalParserInterpretation().interpret("G()(al)"));
     }
     public String interpret(String command) {
-        StringBuilder result = new StringBuilder();
-        List<String> split = new ArrayList<>();
+        StringBuilder result = new StringBuilder(command.length());
         for (int i = 0; i < command.length(); i++){
             switch (command.charAt(i)){
-                case 'G': {
-                    result.append("G");
-                    break;
-                }
-                case '(': {
-                    int count = 0;
-                    if (command.charAt(Math.clamp(i + 1, 0, command.length() - 1)) == 'a' && command.charAt(Math.clamp(i + 2, 0, command.length() - 1)) == 'l' && command.charAt(Math.clamp(i + 3, 0, command.length() - 1)) == ')'){
-                        count = 2;
-                    }
-                    if (command.charAt(Math.clamp(i + 1, 0, command.length() - 1)) == ')'){
-                        count++;
-                    }
-                    if (count == 1){
-                        result.append("o");
-                    }
-                    else if (count == 2){
-                        result.append("al");
+                case 'G'-> result.append("G");
+                case '('-> {
+                    switch (command.charAt(i + 1)){
+                        case 'a' -> result.append("al");
+                        case ')' -> result.append("o");
                     }
                 }
             }
