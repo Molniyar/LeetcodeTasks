685dfb57c49716e8b597e79e273ed292dcd102f6
Yaroslav Savin <molniyar11@gmail.com>
2024-03-16 17:34:23 +0400

    3019 Number of changing keys

---
+++ src/Task3019_NumberOfChangingKeys/NumberOfChangingKeys.java
@@ -0,0 +1,19 @@
+package Task3019_NumberOfChangingKeys;
+
+public class NumberOfChangingKeys {
+    public static void main(String[] args) {
+        System.out.println(new NumberOfChangingKeys().countKeyChanges(""));
+    }
+    public int countKeyChanges(String s) {
+        s = s.toLowerCase();
+        char key = s.charAt(0);
+        int result = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) != key){
+                key = s.charAt(i);
+                result++;
+            }
+        }
+        return result;
+    }
+}
