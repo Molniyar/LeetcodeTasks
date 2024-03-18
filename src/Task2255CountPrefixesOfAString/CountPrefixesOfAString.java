92f03309054d1d43d40fcf3bd55e82e140cc37ad
Yaroslav Savin <molniyar11@gmail.com>
2024-03-18 17:59:47 +0400

    2255 Count prefixes of aString

---
+++ src/Task2255CountPrefixesOfAString/CountPrefixesOfAString.java
@@ -0,0 +1,27 @@
+package Task2255CountPrefixesOfAString;
+
+public class CountPrefixesOfAString {
+    public static void main(String[] args) {
+        //"feh","w","w","lwd","c","s","vk","zwlv","n","w","sw","qrd","w","w","mqe","w","w","w","gb","w","qy","xs","br","w","rypg","wh","g","w","w","fh","w","w","sccy"
+        System.out.println(new CountPrefixesOfAString().countPrefixes(new String[]{"feh","w","w","lwd","c","s","vk","zwlv","n","w","sw","qrd","w","w","mqe","w","w","w","gb","w","qy","xs","br","w","rypg","wh","g","w","w","fh","w","w","sccy"}, "w"));
+    }
+    public int countPrefixes(String[] words, String s) {
+        int result = 0;
+        for (int i = 0; i < words.length; i++){
+            boolean isPrefix = true;
+            if (words[i].length() <= s.length()) {
+                for (int j = 0; j < words[i].length() && j < s.length(); j++) {
+                    if (words[i].charAt(j) != s.charAt(j)) {
+                        isPrefix = false;
+                        break;
+                    }
+
+                }
+                if (isPrefix) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
