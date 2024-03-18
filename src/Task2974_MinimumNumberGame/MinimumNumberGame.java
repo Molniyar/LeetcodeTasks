5c9701335d84263d7d201c27c3e52cccd483b364
Yaroslav Savin <molniyar11@gmail.com>
2024-03-18 19:08:26 +0400

    2974 Minimum number game упрощён код

--- src/Task2974_MinimumNumberGame/MinimumNumberGame.java
+++ src/Task2974_MinimumNumberGame/MinimumNumberGame.java
@@ -7,32 +7,18 @@ import java.util.List;
 public class MinimumNumberGame {
     public static void main(String[] args) {
         int[] test = {5,4,2,3};
-        System.out.println(Arrays.toString(new MinimumNumberGame().numberGame(test)));
+        int[] test1 = {1,5,8,3,5,2,2,7};
+        System.out.println(Arrays.toString(new MinimumNumberGame().numberGame(test1)));
     }
     public int[] numberGame(int[] nums) {
-        int[] result = new int[nums.length];
-        for (int x = 0; x < nums.length; x+=2) {
-            int alice = 100;
-            int indexA = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if (alice > nums[i]) {
-                    alice = nums[i];
-                    indexA = i;
-                }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i+=2){
+            if (nums[i] < nums[i + 1]){
+                int keep = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = keep;
             }
-            nums[indexA] = 101;
-            int bob = 100;
-            int indexB = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if (bob > nums[i]) {
-                    bob = nums[i];
-                    indexB = i;
-                }
-            }
-            nums[indexB] = 101;
-            result[x] = bob;
-            result[x + 1] = alice;
         }
-        return result;
+        return nums;
     }
 }
