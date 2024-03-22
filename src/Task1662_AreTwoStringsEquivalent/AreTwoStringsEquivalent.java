ea801df9a7c2698ddaadac68e22c5d4c1484e34d
Yaroslav Savin <molniyar11@gmail.com>
2024-03-22 14:40:06 +0400

    1662 Are two strings equivalent упрощён код

--- src/Task1662_AreTwoStringsEquivalent/AreTwoStringsEquivalent.java
+++ src/Task1662_AreTwoStringsEquivalent/AreTwoStringsEquivalent.java
@@ -2,19 +2,33 @@ package Task1662_AreTwoStringsEquivalent;
 
 public class AreTwoStringsEquivalent {
     public static void main(String[] args) {
-        String[] word1 = {"ab", "c"};
-        String[] word2 = {"a", "bc"};
+        String[] word1 = {"abc","d","defg"};
+        String[] word2 = {"abcddef"};
         System.out.println(new AreTwoStringsEquivalent().arrayStringsAreEqual(word1, word2));
     }
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder first = new StringBuilder();
-        for (String s : word1) {
-            first.append(s);
+        boolean result = true;
+        int count = 0;
+        for (int i = 0, j = 0, i2 = 0, j2 = 0;; j++, j2++){
+            if ( i >= word1.length || i2 >= word2.length){
+                if (!(i >= word1.length && i2 >= word2.length)){
+                    result = false;
+                }
+                break;
+            }
+            if (word1[i].charAt(j) != word2[i2].charAt(j2)){
+                result = false;
+                break;
+            }
+            if (j == word1[i].length() - 1){
+                j = -1;
+                i++;
+            }
+            if (j2 == word2[i2].length() - 1){
+                j2 = -1;
+                i2++;
+            }
         }
-        StringBuilder second = new StringBuilder();
-        for (String s : word2) {
-            second.append(s);
-        }
-        return first.toString().contentEquals(second);
+        return result;
     }
 }
