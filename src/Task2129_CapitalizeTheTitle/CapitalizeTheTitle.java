a520fe27dfdd4a70188d867c9fbbacf0fca53fe6
Yaroslav Savin <molniyar11@gmail.com>
2024-03-23 14:10:43 +0400

    2129 Capitalize the title упрощён код

--- src/Task2129_CapitalizeTheTitle/CapitalizeTheTitle.java
+++ src/Task2129_CapitalizeTheTitle/CapitalizeTheTitle.java
@@ -2,22 +2,38 @@ package Task2129_CapitalizeTheTitle;
 
 public class CapitalizeTheTitle {
     public static void main(String[] args) {
-        System.out.println(new CapitalizeTheTitle().capitalizeTitle("First leTTeR of EACH Word"));
+        System.out.println(new CapitalizeTheTitle().capitalizeTitle("L hV"));
     }
     public String capitalizeTitle(String title) {
-        String[] split = title.split(" ");
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < split.length; i++){
-            StringBuilder base = new StringBuilder(split[i].toLowerCase());
-            if (split[i].length() < 3){
-                result.append(base);
+        for (int i = 0; i < title.length(); i++){
+            boolean needsToBeToUpperCase = false;
+            char char_ = title.charAt(i);
+            if (char_ != ' ' && (i == 0 || title.charAt(i - 1) == ' ')) {
+                for (int j = i; j < i + 3 && j < title.length(); j++) {
+                    if (title.charAt(j) == ' '){
+                        break;
+                    }
+                    if (j == i + 2) {
+                        needsToBeToUpperCase = true;
+                        break;
+                    }
+                }
+                if (needsToBeToUpperCase){
+                    if (char_ >= 'a' && char_ <='z') {
+                        result.append((char) (char_ + ('A' - 'a')));
+                    }
+                    else result.append(char_);
+                }
+                else if (char_ >= 'A' && char_ <='Z'){
+                    result.append((char) (char_ - ('A' - 'a')));
+                }
+                else result.append(char_);
             }
-            else{
-                result.append((base.charAt(0) + "").toUpperCase()).append(base.deleteCharAt(0));
+            else if (char_ >= 'A' && char_ <='Z'){
+                result.append((char) (char_ - ('A' - 'a')));
             }
-            if (i < split.length - 1){
-                result.append(" ");
-            }
+            else result.append(char_);
         }
         return result.toString();
     }
