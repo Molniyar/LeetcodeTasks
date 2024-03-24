c961303a52c0f1f547a0eb3a33a77a753db17c01
Yaroslav Savin <molniyar11@gmail.com>
2024-03-24 14:35:42 +0400

    1512 Number of good pairs упрощён код

--- src/Task1512_NumberOfGoodPairs/NumberOfGoodPairs.java
+++ src/Task1512_NumberOfGoodPairs/NumberOfGoodPairs.java
@@ -1,25 +1,34 @@
 package Task1512_NumberOfGoodPairs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberOfGoodPairs {
     public static void main(String[] args) {
         int[] test0 = {1,2,3,1,1,3};
-        int[] test1 = {};
-        int[] test2 = {};
-        int[] test3 = {};
+        int[] test1 = {1,1,1};
+        int[] test2 = {3,3};
+        int[] test3 = {1,1,1,2,3,3};
         int[] test4 = {};
 
         int[] problem0 = {};
 
-        System.out.println(new NumberOfGoodPairs().numIdenticalPairs(test0));
+        System.out.println(new NumberOfGoodPairs().numIdenticalPairs(test2));
     }
     public int numIdenticalPairs(int[] nums) {
-        int result = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++){
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[i] == nums[j]){
-                    result++;
-                }
+            if (!hashMap.containsKey(nums[i])){
+                hashMap.put(nums[i], 1);
             }
+            else {
+                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
+            }
+        }
+        int result = 0;
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()){
+            int x = entry.getValue();
+            result += x * (x - 1) / 2;
         }
         return result;
     }
