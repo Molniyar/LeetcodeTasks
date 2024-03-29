8d64813cdb60d3b45ce9865853720052727c75ac
Yaroslav Savin <molniyar11@gmail.com>
2024-03-29 18:41:21 +0400

    463 Island perimeter

---
+++ src/Page04/Task0463/IslandPerimeter.java
@@ -0,0 +1,35 @@
+package Page04.Task0463;
+
+public class IslandPerimeter {
+    public static void main(String[] args) {
+        System.out.println(new IslandPerimeter().islandPerimeter(new int[][]{{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}}));
+        System.out.println(new IslandPerimeter().islandPerimeter(new int[][]{{1, 0}}));
+    }
+    public int islandPerimeter(int[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length; j++){
+                if (grid[i][j] == 1) {
+                    int upChunk = i - 1;
+                    if (upChunk < 0 || grid[upChunk][j] == 0) {
+                        result++;
+                    }
+                    int downChunk = i + 1;
+                    if (downChunk >= grid.length || grid[downChunk][j] == 0) {
+                        result++;
+                    }
+
+                    int leftChunk = j - 1;
+                    if (leftChunk < 0 || grid[i][leftChunk] == 0) {
+                        result++;
+                    }
+                    int rightChunk = j + 1;
+                    if (rightChunk >= grid[i].length || grid[i][rightChunk] == 0) {
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
