f29ae9d77b6992140fcdf574fc4ce42c2e227332
Yaroslav Savin <molniyar11@gmail.com>
2024-04-04 20:34:58 +0400

    2413 Smallest even multiple

---
+++ src/Page24/Task2413/SmallestEvenMultiple.java
@@ -0,0 +1,13 @@
+package Page24.Task2413;
+
+public class SmallestEvenMultiple {
+    public static void main(String[] args) {
+
+    }
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0){
+            return n;
+        }
+        else return n * 2;
+    }
+}
