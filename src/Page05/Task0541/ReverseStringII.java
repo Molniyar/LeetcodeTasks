17cd42b2f275d0ebe81e69b01dd76523c0a08c90
Yaroslav Savin <molniyar11@gmail.com>
2024-04-14 18:34:33 +0400

    541 Reverse string II упрощён код

--- src/Page05/Task0541/ReverseStringII.java
+++ src/Page05/Task0541/ReverseStringII.java
@@ -2,20 +2,26 @@ package Page05.Task0541;
 
 public class ReverseStringII {
     public static void main(String[] args) {
+        System.out.println(new ReverseStringII().reverseStr("cgcgcgcgcgcg", 2));
         System.out.println(new ReverseStringII().reverseStr("a", 2));
+        //System.out.println(reverse(new char[] {'a','b','c'}, 2));
     }
     public String reverseStr(String s, int k) {
         char[] sArray = s.toCharArray();
-        for (int i = 0; i < s.length(); i += k * 2){
-            StringBuilder sb = new StringBuilder();
-            for (int j = i; j < k + i && j < s.length(); j++){
-                sb.append(sArray[j]);
-            }
-            sb.reverse();
-            for (int j = 0; j < sb.length(); j++){
-                sArray[j + i] = sb.charAt(j);
-            }
+        for (int i = 0; i < s.length(); i+= k*2){
+            reverse(sArray, i,k + i);
         }
         return new String(sArray);
+    }
+    public static void reverse(char[] chars, int min, int max) {
+        int cut = max-1;
+        if (chars.length < max){
+            cut = chars.length-1;
+        }
+        for (int i = min, j = cut; i < j; i++, j--){
+            char keep = chars[j];
+            chars[j] = chars[i];
+            chars[i] = keep;
+        }
     }
 }
