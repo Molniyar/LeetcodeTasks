113ecbf829c2bfab8626ce7025ebaecd6a49c345
Yaroslav Savin <molniyar11@gmail.com>
2024-04-15 19:17:20 +0400

    557 Reverse words in a string III упрощён код

--- src/Page05/Task0557/ReverseWordsInAStringIII.java
+++ src/Page05/Task0557/ReverseWordsInAStringIII.java
@@ -1,19 +1,32 @@
 package Page05.Task0557;
 
+import java.util.Arrays;
+
 public class ReverseWordsInAStringIII {
     public static void main(String[] args) {
         System.out.println(new ReverseWordsInAStringIII().reverseWords("Let's take LeetCode contest"));
     }
     public String reverseWords(String s) {
-        String[] split = s.split(" ");
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < split.length; i++){
-            StringBuilder str = new StringBuilder(split[i]);
-            result.append(str.reverse());
-            if (i < split.length - 1){
-                result.append(" ");
+        char[] chars = s.toCharArray();
+        int startIndex = 0;
+        for (int i = 0; i < s.length();i++){
+            if (s.charAt(i) == ' '){
+                reverse(chars,startIndex,i);
+                startIndex = i + 1;
             }
         }
-        return result.toString();
+        reverse(chars,startIndex,s.length());
+        return new String(chars);
+    }
+    public static void reverse(char[] chars, int min, int max) {
+        int cut = max-1;
+        if (chars.length < max){
+            cut = chars.length-1;
+        }
+        for (int i = min, j = cut; i < j; i++, j--){
+            char keep = chars[j];
+            chars[j] = chars[i];
+            chars[i] = keep;
+        }
     }
 }
