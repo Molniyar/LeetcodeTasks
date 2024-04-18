8c996dbeda025d505b01cf0f627ff96f200f62f3
Yaroslav Savin <molniyar11@gmail.com>
2024-04-18 17:35:04 +0400

    Replace all digits with characters

---
+++ src/Page18/Task1844/ReplaceAllDigitsWithCharacters.java
@@ -0,0 +1,17 @@
+package Page18.Task1844;
+
+public class ReplaceAllDigitsWithCharacters {
+    public static void main(String[] args) {
+        System.out.println(new ReplaceAllDigitsWithCharacters().replaceDigits("a1c1e1"));
+    }
+    public String replaceDigits(String s) {
+        char[] str = s.toCharArray();
+        for (int i = 0; i < str.length;i++){
+            char char_ = str[i];
+            if (char_ >= '0' && char_ <= '9'){
+                str[i] = (char)(str[i-1]+(char_-48));
+            }
+        }
+        return new String(str);
+    }
+}
