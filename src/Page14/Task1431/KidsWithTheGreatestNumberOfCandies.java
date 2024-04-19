c1017fa79c5c8226be73087dd05b81c93fca0bb2
Yaroslav Savin <molniyar11@gmail.com>
2024-04-19 16:56:40 +0400

    1431 Kids with the greatest number of candies упрощён код

--- src/Page14/Task1431/KidsWithTheGreatestNumberOfCandies.java
+++ src/Page14/Task1431/KidsWithTheGreatestNumberOfCandies.java
@@ -9,17 +9,15 @@ public class KidsWithTheGreatestNumberOfCandies {
         System.out.println(new KidsWithTheGreatestNumberOfCandies().kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3));
     }
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> result = new ArrayList<>();
-        for (int i = 0; i < candies.length; i++){
-            int x = candies[i] + extraCandies;
-            int count = 0;
-            for (int j = 0; j < candies.length; j++){
-                if (x >= candies[j]){
-                    count++;
-                }
-                else break;
+        List<Boolean> result = new ArrayList<>(candies.length);
+        int biggestNOfCandies = 0;
+        for (int n : candies) {
+            if (n > biggestNOfCandies) {
+                biggestNOfCandies = n;
             }
-            result.add(count == candies.length);
+        }
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= biggestNOfCandies);
         }
         return result;
     }
