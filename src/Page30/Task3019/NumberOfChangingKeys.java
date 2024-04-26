b3e52d74b8b4289a8b8794cb2f9ef50c63f89f20
Yaroslav Savin <molniyar11@gmail.com>
2024-04-26 18:37:22 +0400

    3019 Number of changing keys упрощён код

--- src/Page30/Task3019/NumberOfChangingKeys.java
+++ src/Page30/Task3019/NumberOfChangingKeys.java
@@ -2,15 +2,19 @@ package Page30.Task3019;
 
 public class NumberOfChangingKeys {
     public static void main(String[] args) {
-        System.out.println(new NumberOfChangingKeys().countKeyChanges(""));
+        System.out.println(new NumberOfChangingKeys().countKeyChanges("AaAaAaaA"));
     }
     public int countKeyChanges(String s) {
-        s = s.toLowerCase();
         char key = s.charAt(0);
+        if (key >= 'A' && key <= 'Z'){key -= (char) ('A'-'a');}
         int result = 0;
         for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) != key){
-                key = s.charAt(i);
+            char char_ = s.charAt(i);
+            if (char_ >= 'A' && char_ <= 'Z'){
+                char_ -= (char) ('A'-'a');
+            }
+            if (char_ != key){
+                key = char_;
                 result++;
             }
         }
