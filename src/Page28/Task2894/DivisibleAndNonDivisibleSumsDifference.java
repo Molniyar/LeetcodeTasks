452e49b9caca404748b006a942c83e76f45cf3e9
Yaroslav Savin <molniyar11@gmail.com>
2024-04-27 11:59:46 +0400

    2894 Divisible and non divisible sums difference

--- src/Page28/Task2894/DivisibleAndNonDivisibleSumsDifference.java
+++ src/Page28/Task2894/DivisibleAndNonDivisibleSumsDifference.java
@@ -5,14 +5,17 @@ public class DivisibleAndNonDivisibleSumsDifference {
         System.out.println(new DivisibleAndNonDivisibleSumsDifference().differenceOfSums(10, 3));
     }
     public int differenceOfSums(int n, int m) {
-        int notDivisible = 0;
+        int all = 0;
+        for (int i = 1; i <= n; i++){
+            all += i;
+        }
+
         int divisible = 0;
         for (int i = 1; i <= n; i++){
             if (i % m == 0){
                 divisible += i;
             }
-            else notDivisible += i;
         }
-        return notDivisible - divisible;
+        return all - divisible - divisible;
     }
 }
