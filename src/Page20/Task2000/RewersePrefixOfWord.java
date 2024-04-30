2dc8a2498e030fe90842902d5a5d2bcb1bf4f8e0
Yaroslav Savin <molniyar11@gmail.com>
2024-04-30 18:31:02 +0400

    2000 reverse prefix of word упрощён код

--- src/Page20/Task2000/RewersePrefixOfWord.java
+++ src/Page20/Task2000/RewersePrefixOfWord.java
@@ -5,8 +5,19 @@ public class RewersePrefixOfWord {
         System.out.println(new RewersePrefixOfWord().reversePrefix("abcdefd", 'd'));
     }
     public String reversePrefix(String word, char ch) {
-        int firstOccurence = word.indexOf(ch);
-        StringBuilder left = new StringBuilder(word);
-        return left.delete(firstOccurence + 1, left.length()).reverse().append(word.substring(firstOccurence + 1)).toString();
+        char[] chars = word.toCharArray();
+        int char_ = 0;
+        for (int i = 0;i < chars.length;i++){
+            if (chars[i] == ch){
+                char_ = i;
+                break;
+            }
+        }
+        for (int i = 0, j = char_; i < j;i++,j--){
+            char keep = chars[i];
+            chars[i] = chars[j];
+            chars[j] = keep;
+        }
+        return new String(chars);
     }
 }
