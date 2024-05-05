eb83adecc42b092028f8baa8406ff993e4a25f62
Yaroslav Savin <molniyar11@gmail.com>
2024-05-05 16:10:20 +0400

    1455 Index of word with prefix упрощён код

--- src/Page14/Task1455/IndexOfWordWithPrefix.java
+++ src/Page14/Task1455/IndexOfWordWithPrefix.java
@@ -7,7 +7,7 @@ public class IndexOfWordWithPrefix {
     public int isPrefixOfWord(String sentence, String searchWord) {
         String[] split = sentence.split(" ");
         for (int i = 0; i < split.length; i++){
-            if (split[i].length() >= searchWord.length() && split[i].substring(0, searchWord.length()).equals(searchWord)){
+            if (split[i].length() >= searchWord.length() && split[i].startsWith(searchWord)){
                 return i + 1;
             }
         }
