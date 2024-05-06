e653c11ab4f22ab37e39ff50dd61b01c51147f86
Yaroslav Savin <molniyar11@gmail.com>
2024-05-06 21:41:54 +0400

    2864 Maximum odd binary number упрощён код

--- src/Page28/Task2864/MaximumOddBinaryNumber.java
+++ src/Page28/Task2864/MaximumOddBinaryNumber.java
@@ -5,23 +5,27 @@ public class MaximumOddBinaryNumber  {
         System.out.println(new MaximumOddBinaryNumber().maximumOddBinaryNumber("1011110"));
     }
     public String maximumOddBinaryNumber(String s) {
-        StringBuilder result = new StringBuilder();
+        char[] chars = new char[s.length()];
         int bitCount = -1;
         for (int i = 0; i < s.length(); i++){
             char char_ = s.charAt(i);
+            chars[i] = '0';
             if (char_ == '1'){
                 bitCount++;
             }
         }
-        char add = '1';
+        boolean hasBit = true;
+        int c = 0;
         for (int i = 0; i < s.length() - 1; i++){
             if (bitCount == 0){
-                add = '0';
+                hasBit = false;
             }
-            result.append(add);
+            if (hasBit){
+                chars[i] = '1';
+            }
             bitCount--;
         }
-        result.append('1');
-        return result.toString();
+        chars[s.length()-1] = ('1');
+        return new String(chars);
     }
 }
