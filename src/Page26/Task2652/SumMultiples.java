03eddd264a95a272dd8615d1ffa62c3a854fe8aa
Yaroslav Savin <molniyar11@gmail.com>
2024-05-07 21:48:41 +0400

    2652 Sum multiples

--- src/Page26/Task2652/SumMultiples.java
+++ src/Page26/Task2652/SumMultiples.java
@@ -2,7 +2,7 @@ package Page26.Task2652;
 
 public class SumMultiples {
     public static void main(String[] args) {
-        System.out.println(new SumMultiples().sumOfMultiples(7));
+        System.out.println(new SumMultiples().sumOfMultiples(10));
     }
     public int sumOfMultiples(int n) {
         int result = 0;
@@ -11,6 +11,10 @@ public class SumMultiples {
                 result += i;
             }
         }
-        return result;
+        int sum = n * (n+1)/2;
+        return divisibleSum(n,3);// + divisibleSum(n,5) + divisibleSum(n,7);
+    }
+    public int divisibleSum(int n, int divide){
+        return n/divide * (n/divide+1);
     }
 }
