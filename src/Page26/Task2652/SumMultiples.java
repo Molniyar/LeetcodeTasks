a4d0bb90ae86c5cf5044d128f10054f86512c768
Yaroslav Savin <molniyar11@gmail.com>
2024-05-09 19:48:13 +0400

    2652 Sum multiples упрощён код

--- src/Page26/Task2652/SumMultiples.java
+++ src/Page26/Task2652/SumMultiples.java
@@ -2,19 +2,14 @@ package Page26.Task2652;
 
 public class SumMultiples {
     public static void main(String[] args) {
-        System.out.println(new SumMultiples().sumOfMultiples(10));
+        System.out.println(new SumMultiples().sumOfMultiples(7));
+        System.out.println(divisibleSum(10,2));
     }
     public int sumOfMultiples(int n) {
-        int result = 0;
-        for (int i = 1; i <= n; i++){
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
-                result += i;
-            }
-        }
-        int sum = n * (n+1)/2;
-        return divisibleSum(n,3);// + divisibleSum(n,5) + divisibleSum(n,7);
+        int minus = divisibleSum(n,3*5) + divisibleSum(n,3*7) + divisibleSum(n,5*7) - divisibleSum(n,3*5*7);
+        return divisibleSum(n,3) + divisibleSum(n,5) + divisibleSum(n,7) - minus;
     }
-    public int divisibleSum(int n, int divide){
-        return n/divide * (n/divide+1);
+    public static int divisibleSum(int n, int divide){
+        return n/divide * (n/divide+1)/2*divide;
     }
 }
