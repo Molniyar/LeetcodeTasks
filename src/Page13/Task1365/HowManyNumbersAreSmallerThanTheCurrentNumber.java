6063da3db1e489efc47d5d08548d4991acd334f3
Yaroslav Savin <molniyar11@gmail.com>
2024-05-11 21:28:38 +0400

    1365 How many numbers are smaller than the current number упрорщён код

--- src/Page13/Task1365/HowManyNumbersAreSmallerThanTheCurrentNumber.java
+++ src/Page13/Task1365/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -4,9 +4,8 @@ import java.util.Arrays;
 
 public class HowManyNumbersAreSmallerThanTheCurrentNumber {
     public static void main(String[] args) {
-         System.out.println(getIndexOfNum(new int[]{1, 2, 2, 3, 8}, 8));
-        System.out.println(getIndexOfNum2(new int[]{1, 2, 2, 3, 8}, 8));
-        System.out.println(Arrays.toString(new HowManyNumbersAreSmallerThanTheCurrentNumber().smallerNumbersThanCurrent(new int[]{8, 1, 2, 2, 3})));
+        System.out.println(getIndexOfNum(new int[]{1, 2, 2, 3, 8}, 8));
+        System.out.println(Arrays.toString(new HowManyNumbersAreSmallerThanTheCurrentNumber().smallerNumbersThanCurrent2(new int[]{8, 1, 2, 2, 3})));
     }
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] sortedNums = nums.clone();
@@ -29,12 +28,20 @@ public class HowManyNumbersAreSmallerThanTheCurrentNumber {
         }
         return max;
     }
-    public static int getIndexOfNum2(int[] sortedNums, int num){
-        for (int i = 0; i < sortedNums.length; i++){
-            if (sortedNums[i] == num){
-                return i;
-            }
+
+    public int[] smallerNumbersThanCurrent2(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int[] numArr = new int[101];
+        for (int num : nums) {
+            numArr[num]++;
         }
-        return -1;
+        for (int i = 0; i < nums.length; i++){
+            for (int j = nums[i]; j >=0; j--){
+                result[i]+=numArr[j];
+            }
+            result[i] -= numArr[nums[i]];
+        }
+        return result;
     }
 }
