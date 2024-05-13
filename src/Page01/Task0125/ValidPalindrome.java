e22042c921f588d940b0512c3c88a5adea211be4
Yaroslav Savin <molniyar11@gmail.com>
2024-05-13 20:58:15 +0400

    125 Valid palindrome упрощён код

--- src/Page01/Task0125/ValidPalindrome.java
+++ src/Page01/Task0125/ValidPalindrome.java
@@ -2,45 +2,61 @@ package Page01.Task0125;
 
 public class ValidPalindrome {
     public static void main(String[] args) {
-        System.out.println(new ValidPalindrome().isPalindrome("\\\"Degenerate Moslem, a cad!\\\" Eva saved a camel so Meta reneged."));
+
+        System.out.println(new ValidPalindrome().isPalindrome(".,"));
     }
     public boolean isPalindrome(String s) {
         int length = s.length();
+
         boolean result = true;
+
         for (int i = 0, j = s.length() - 1;i < j; i++, j--){
+
             char startChar = s.charAt(i);
             char endChar = s.charAt(j);
+
+
             for (;true;){
-                if (! isAlphanumeric(startChar)){
-                    i++;
-                    if (i >= length){
-                        break;
-                    }
-                    startChar = s.charAt(i);
+                if (i > j){return true;}
+                if (isAlphanumeric(startChar)){
+                    break;
                 }
-                else if (! isAlphanumeric(endChar)){
-                    j--;
-                    if (j < 0){
-                        break;
-                    }
-                    endChar = s.charAt(j);
+                i++;
+                if (i >= length){
+                    break;
                 }
-                else if (startChar >= 'A' && startChar <= 'Z'){
-                    startChar = (char)(startChar - ('A' - 'a'));
-                }
-                else if (endChar >= 'A' && endChar <= 'Z'){
-                    endChar = (char)(endChar - ('A' - 'a'));
-                }
-                else break;
+                startChar = s.charAt(i);
             }
+
+            for (;true;){
+                if (i > j){return true;}
+                if (isAlphanumeric(endChar)){
+                    break;
+                }
+                j--;
+                if (j < 0){
+                    break;
+                }
+                endChar = s.charAt(j);
+
+            }
+            if (startChar >= 'A' && startChar <= 'Z'){
+                startChar = (char)(startChar - ('A' - 'a'));
+            }
+            if (endChar >= 'A' && endChar <= 'Z'){
+                endChar = (char)(endChar - ('A' - 'a'));
+            }
+
             if (startChar != endChar) {
                 result = false;
                 break;
             }
+
             if (i == j){
                 break;
             }
         }
+
         return result;
     }
     public static boolean isAlphanumeric(char c) {
