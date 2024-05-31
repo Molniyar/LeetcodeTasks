b2369e6f81f6f2f42f3a8dac2854b110052f4129
Yaroslav Savin <molniyar11@gmail.com>
2024-05-31 17:39:38 +0400

    2119 A number after a double reversal

---
+++ src/Page21/Task2119/ANumberAfterADoubleReversal.java
@@ -0,0 +1,13 @@
+package Page21.Task2119;
+
+public class ANumberAfterADoubleReversal {
+    public static void main(String[] args) {
+
+    }
+    public boolean isSameAfterReversals(int num) {
+        if (num > 0 && num % 10 == 0){
+            return false;
+        }
+        return true;
+    }
+}
