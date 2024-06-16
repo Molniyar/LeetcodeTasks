2ce05a9aff3183f1251ddca90c06c46576acf9c3
Yaroslav Savin <molniyar11@gmail.com>
2024-06-16 19:48:27 +0400

    2000 reverse prefix simplified code

--- src/Page20/Task2000/RewersePrefixOfWord.java
+++ src/Page20/Task2000/RewersePrefixOfWord.java
@@ -6,14 +6,8 @@ public class RewersePrefixOfWord {
     }
     public String reversePrefix(String word, char ch) {
         char[] chars = word.toCharArray();
-        int char_ = 0;
-        for (int i = 0;i < chars.length;i++){
-            if (chars[i] == ch){
-                char_ = i;
-                break;
-            }
-        }
-        for (int i = 0, j = char_; i < j;i++,j--){
+
+        for (int i = 0, j = word.indexOf(ch); i < j;i++,j--){
             char keep = chars[i];
             chars[i] = chars[j];
             chars[j] = keep;
