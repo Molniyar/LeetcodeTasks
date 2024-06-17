67826399bc3ace5a7e179d6d166a2fd6a439c40b
Yaroslav Savin <molniyar11@gmail.com>
2024-06-17 13:11:57 +0400

    1812 Determine color of a chessboard square simplified code

--- src/Page18/Task1812/DetermineColorOfAChessboardSquare.java
+++ src/Page18/Task1812/DetermineColorOfAChessboardSquare.java
@@ -5,11 +5,6 @@ public class DetermineColorOfAChessboardSquare {
 
     }
     public boolean squareIsWhite(String coordinates) {
-        boolean compare = (coordinates.charAt(0) >>1) <<1 == coordinates.charAt(0);
-        boolean compare2 = ((coordinates.charAt(1) -'a'+1) >>1) <<1 == coordinates.charAt(1) -'a'+1;
-        if (compare == compare2){
-            return false;
-        }
-        return true;
+        return (coordinates.charAt(0) + coordinates.charAt(1))%2 != 0;
     }
 }
