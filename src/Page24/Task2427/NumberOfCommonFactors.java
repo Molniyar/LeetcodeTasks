aa9c2797b67232106e33f96957bd8cde19e38822
Yaroslav Savin <molniyar11@gmail.com>
2024-06-18 19:02:01 +0400

    2427 Number of common factors simplified code

--- src/Page24/Task2427/NumberOfCommonFactors.java
+++ src/Page24/Task2427/NumberOfCommonFactors.java
@@ -5,8 +5,18 @@ public class NumberOfCommonFactors {
 
     }
     public int commonFactors(int a, int b) {
-        int result = 0;
-        for (int i = 1;i <= Math.min(a,b);i++){
+        int endNum = 0;
+        for (int big = Math.max(a,b), min = Math.min(a,b);;){
+            int keep = min;
+            min = big % min;
+            big = keep;
+            if (min == 0){
+                endNum = big;
+                break;
+            }
+        }
+        int result = 1;
+        for (int i = 1;i <= endNum;i++){
             if (a%i == 0 && b%i == 0){result++;}
         }
         return result;
