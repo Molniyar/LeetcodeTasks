67044225c72f7ff79f26fab3c1d7e84cab7b6545
Yaroslav Savin <molniyar11@gmail.com>
2024-06-19 17:35:34 +0400

    1768 Merge strings alternately simplified code

--- src/Page17/Task1768/MergeStringsAlternately.java
+++ src/Page17/Task1768/MergeStringsAlternately.java
@@ -1,21 +1,36 @@
 package Page17.Task1768;
 
+import java.util.Arrays;
+
 public class MergeStringsAlternately {
     public static void main(String[] args) {
-
+        System.out.println(new MergeStringsAlternately().mergeAlternately("", ""));
     }
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder result = new StringBuilder();
+        char[] result = new char[word1.length()+word2.length()];
 
         int i = 0;
-        for (; i < Math.min(word1.length(),word2.length()); i++){
-            result.append(word1.charAt(i));
-            result.append(word2.charAt(i));
+        int a = 0;
+        for (; i < Math.min(word1.length(),word2.length());i++){
+            result[a] = word1.charAt(i);
+            a++;
+            result[a] = word2.charAt(i);
+            a++;
         }
 
-        if (word1.length() > word2.length()){result.append(word1.substring(i)); }
-        if (word1.length() < word2.length()){result.append(word2.substring(i)); }
+        if (word1.length() > word2.length()){
+            for (; i < word1.length();i++){
+                result[a] = word1.charAt(i);
+                a++;
+            }
+        }
+        if (word1.length() < word2.length()){
+            for (; i < word2.length();i++){
+                result[a] = word2.charAt(i);
+                a++;
+            }
+        }
 
-        return result.toString();
+        return new String(result);
     }
 }
