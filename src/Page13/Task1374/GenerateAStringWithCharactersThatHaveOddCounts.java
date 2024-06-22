d9df35087d50ac51dac18fecb702e05d6fa8ab45
Yaroslav Savin <molniyar11@gmail.com>
2024-06-22 16:47:06 +0400

    1374 Generate a string with characters that have  odd counts

--- src/Page13/Task1374/GenerateAStringWithCharactersThatHaveOddCounts.java
+++ src/Page13/Task1374/GenerateAStringWithCharactersThatHaveOddCounts.java
@@ -1,22 +1,17 @@
 package Page13.Task1374;
 
+import java.util.Arrays;
+
 public class GenerateAStringWithCharactersThatHaveOddCounts {
     public static void main(String[] args) {
 
     }
     public String generateTheString(int n) {
-        StringBuilder result = new StringBuilder(n);
-        boolean shouldBeAdded = false;
+        char[] result = new char[n];
+        Arrays.fill(result, 'a');
         if (n % 2 == 0){
-            shouldBeAdded = true;
-            n--;
+            result[n-1] = 'b';
         }
-        for (int i = 0; i < n; i++){
-            result.append('a');
-        }
-        if (shouldBeAdded){
-            result.append('b');
-        }
-        return result.toString();
+        return new String(result);
     }
 }
