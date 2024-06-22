1fe329f3ca6bcb7848c12a3ba3f927c45f6fd065
Yaroslav Savin <molniyar11@gmail.com>
2024-06-22 16:46:26 +0400

    338 Counting bits

--- src/Page03/Task0338/CountingBits.java
+++ src/Page03/Task0338/CountingBits.java
@@ -6,8 +6,18 @@ public class CountingBits {
     }
     public int[] countBits(int n) {
         int[] result = new int[n+1];
-        for (int i = 0; i < n+1; i++){
-            result[i] = Integer.bitCount(i);
+        result[0] = 0;
+
+        int Int = 1;
+        for (;Int < n;n <<= 1){}
+
+        int k = 1;
+
+        for (int i = 0; i < result.length; i++){
+            for (int j = k; j < k+k && j < result.length; j++){
+                result[j] = result[j-k]+1;
+            }
+            k<<=1;
         }
         return result;
     }
