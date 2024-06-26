f870c5b1b15bc13985dca76cd09d06d6362c55ce
Yaroslav Savin <molniyar11@gmail.com>
2024-06-26 14:12:35 +0400

    2773 Neither minimum nor maximum simplified code

--- src/Page27/Task2733/NeitherMinimumNorMaximum.java
+++ src/Page27/Task2733/NeitherMinimumNorMaximum.java
@@ -6,18 +6,19 @@ import java.util.Arrays;
 
 public class NeitherMinimumNorMaximum {
     public static void main(String[] args) {
-        System.out.println(new NeitherMinimumNorMaximum().findNonMinOrMax(new int[] {1,4,2,3}));
+        System.out.println(new NeitherMinimumNorMaximum().findNonMinOrMax(new int[] {4,2,1,3}));
     }
     public int findNonMinOrMax(int[] nums) {
         if (nums.length < 3){return -1;}
 
-        Arrays.sort(nums);
+        int num1 = nums[0];
+        int num2 = nums[1];
+        int num3 = nums[2];
 
-        for (int i = 1; i < nums.length-1; i++){
-            if (nums[i] > nums[0]){
-                return nums[i];
-            }
-        }
+        if (num1 > num2 == num1 < num3){return num1;}
+        if (num2 > num1 == num2 < num3){return num2;}
+        if (num3 > num2 == num3 < num1){return num3;}
+
         return -1;
     }
 }
