52f39dad502d24651c731724ca874a81f727fe88
molniyar <molniyar11@gmail.com>
2024-06-28 22:12:52 +0400

    2264 Largest 3-Same-Digit number in string simplified code

--- src/Page22/Task2264/Largest3_Same_DigitNumberInString.java
+++ src/Page22/Task2264/Largest3_Same_DigitNumberInString.java
@@ -5,20 +5,28 @@ public class Largest3_Same_DigitNumberInString {
         System.out.println(new Largest3_Same_DigitNumberInString().largestGoodInteger("3200014888"));
     }
     public String largestGoodInteger(String num) {
-        int result = -1;
+        String res = "";
+        char resChar = 0;
         int count = 0;
-        int keep = 0;
-        int i = 0;
-        for (; i < num.length(); i++){
-            if (i < num.length()-2 && num.charAt(i) == num.charAt(i+1) && num.charAt(i+1)==num.charAt(i+2)){
-                if (num.charAt(i) - 48 > result){
-                    result = num.charAt(i) - 48;
-                    i+=2;
-                    keep = i;
-                }
+        char keep = 0;
+
+        for (int i = 0; i < num.length(); i++){
+            char char_ = num.charAt(i);
+            if (keep != char_){
+                count = 1;
+                keep = char_;
+
+                continue;
+            }
+
+            count++;
+
+            if (count == 3 && char_ > resChar){
+                res = num.substring(i-2,i+1);
+                resChar = char_;
             }
         }
-        if (result == -1){return "";}
-        return num.substring(keep - 2,keep + 1);
+
+        return res;
     }
 }
