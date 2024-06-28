1c57d621dc0d9eb22c1602ad468e37f83b3a4335
molniyar <molniyar11@gmail.com>
2024-06-28 22:28:06 +0400

    2864 Maximum odd binary number

--- src/Page28/Task2864/MaximumOddBinaryNumber.java
+++ src/Page28/Task2864/MaximumOddBinaryNumber.java
@@ -1,31 +1,27 @@
 package Page28.Task2864;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MaximumOddBinaryNumber  {
     public static void main(String[] args) {
         System.out.println(new MaximumOddBinaryNumber().maximumOddBinaryNumber("1011110"));
     }
     public String maximumOddBinaryNumber(String s) {
-        char[] chars = new char[s.length()];
+        char[] res = new char[s.length()];
+
         int bitCount = -1;
         for (int i = 0; i < s.length(); i++){
-            char char_ = s.charAt(i);
-            chars[i] = '0';
-            if (char_ == '1'){
+            if (s.charAt(i) == '1'){
                 bitCount++;
             }
         }
-        boolean hasBit = true;
-        int c = 0;
-        for (int i = 0; i < s.length() - 1; i++){
-            if (bitCount == 0){
-                hasBit = false;
-            }
-            if (hasBit){
-                chars[i] = '1';
-            }
-            bitCount--;
-        }
-        chars[s.length()-1] = ('1');
-        return new String(chars);
+
+        res[s.length()-1] = '1';
+
+        Arrays.fill(res,0,bitCount+1,'1');
+        Arrays.fill(res,bitCount,res.length-1,'0');
+
+        return new String(res);
     }
 }
