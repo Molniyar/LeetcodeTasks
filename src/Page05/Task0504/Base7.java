7bad23dbfb8b29e4b247961f9f78453cb94dab53
Yaroslav Savin <molniyar11@gmail.com>
2024-06-30 17:39:13 +0400

    504 Base 7

---
+++ src/Page05/Task0504/Base7.java
@@ -0,0 +1,10 @@
+package Page05.Task0504;
+
+public class Base7 {
+    public static void main(String[] args) {
+
+    }
+    public String convertToBase7(int num) {
+        return Integer.toString(num,7);
+    }
+}
