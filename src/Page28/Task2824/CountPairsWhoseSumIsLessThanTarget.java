623e7223ff76ac9b12959a1b06b9439b1807694e
molniyar <molniyar11@gmail.com>
2024-07-11 15:44:42 +0400

    2824 Count pairs whose sum is less than target simplified code

--- src/Page28/Task2824/CountPairsWhoseSumIsLessThanTarget.java
+++ src/Page28/Task2824/CountPairsWhoseSumIsLessThanTarget.java
@@ -1,6 +1,7 @@
 package Page28.Task2824;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CountPairsWhoseSumIsLessThanTarget {
@@ -14,12 +15,23 @@ public class CountPairsWhoseSumIsLessThanTarget {
         System.out.println(new CountPairsWhoseSumIsLessThanTarget().countPairs(nums, 2));
     }
     public int countPairs(List<Integer> nums, int target) {
-        int result = 0;
-        for (int i = 0; i < nums.size(); i++){
-            for (int j = i +1; j < nums.size(); j++){
-                if (nums.get(i) + nums.get(j) < target){result++;}
+        Collections.sort(nums);
+
+        int count = 0;
+
+        int left = 0;
+        int right = nums.size()-1;
+
+        while (left < right){
+            if (nums.get(left) + nums.get(right) < target){
+                count += right-left;
+                left++;
+            }
+            else {
+                right--;
             }
         }
-        return result;
+        
+        return count;
     }
 }
