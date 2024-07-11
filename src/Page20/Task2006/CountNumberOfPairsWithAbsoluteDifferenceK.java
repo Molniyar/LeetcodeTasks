d1edf5db622251e1c36f61282d00d0719f06c2b1
molniyar <molniyar11@gmail.com>
2024-07-11 15:15:15 +0400

    2006 Count number of pairs with absolute difference k simplified code

--- src/Page20/Task2006/CountNumberOfPairsWithAbsoluteDifferenceK.java
+++ src/Page20/Task2006/CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -1,19 +1,29 @@
 package Page20.Task2006;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CountNumberOfPairsWithAbsoluteDifferenceK {
     public static void main(String[] args) {
         System.out.println(new CountNumberOfPairsWithAbsoluteDifferenceK().countKDifference(new int[]{1,2,2,1}, 1));
     }
     public int countKDifference(int[] nums, int k) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++){
-            int keep = nums[i];
-            for (int j = i+1; j < nums.length; j++){
-                if (keep + k == nums[j] || keep - k == nums[j]){
-                    count++;
-                }
+        Map<Integer,Integer> map = new HashMap<>();
+
+        int res = 0;
+
+        for (int i = 0; i< nums.length; i++){
+            if(map.containsKey(nums[i] - k)){
+                res += map.get(nums[i] - k);
             }
+            if(map.containsKey(nums[i] + k)){
+                res += map.get(nums[i] + k);
+            }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        return count;
+
+        return res;
     }
 }
