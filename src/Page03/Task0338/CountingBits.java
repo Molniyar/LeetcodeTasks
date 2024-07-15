d504cb49068f042cc3440b4363374e9af82f23c9
molniyar <molniyar11@gmail.com>
2024-07-15 20:28:49 +0400

    338 Counting bits simplified code

--- src/Page03/Task0338/CountingBits.java
+++ src/Page03/Task0338/CountingBits.java
@@ -1,23 +1,15 @@
 package Page03.Task0338;
 
+import java.util.Arrays;
+
 public class CountingBits {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(new CountingBits().countBits(100)));
     }
     public int[] countBits(int n) {
         int[] result = new int[n+1];
-        result[0] = 0;
-
-        int Int = 1;
-        for (;Int < n;n <<= 1){}
-
-        int k = 1;
-
-        for (int i = 0; i < result.length; i++){
-            for (int j = k; j < k+k && j < result.length; j++){
-                result[j] = result[j-k]+1;
-            }
-            k<<=1;
+        for (int i = 0; i < n+1; i++){
+            result[i] = Integer.bitCount(i);
         }
         return result;
     }
