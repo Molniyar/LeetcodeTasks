70b9d5be4ea0367f184d17ce1917bbf83452c284
molniyar <molniyar11@gmail.com>
2024-07-15 20:28:23 +0400

    35 Search insert position simplified code

--- src/Page00/Task0035/SearchInsertPosition.java
+++ src/Page00/Task0035/SearchInsertPosition.java
@@ -1,16 +1,28 @@
 package Page00.Task0035;
 
+import java.util.Arrays;
+
 public class SearchInsertPosition {
     public static void main(String[] args) {
 
     }
     public int searchInsert(int[] nums, int target) {
-        for (int i = 0;i < nums.length;i++){
-            if (nums[i] < target){
-                continue;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
             }
-            return i;
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
         }
-        return nums.length;
+
+        return left;
     }
 }
