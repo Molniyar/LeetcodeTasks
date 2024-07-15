06d9ddba50b50ed2d7ed66f98861ef9c0e140ca7
molniyar <molniyar11@gmail.com>
2024-07-15 11:16:13 +0400

    141 Linked list cycle

---
+++ src/Page01/Task0141/LinkedListCycle.java
@@ -0,0 +1,22 @@
+package Page01.Task0141;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/Page01/Task0141/ListNode.java
@@ -0,0 +1,10 @@
+package Page01.Task0141;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
