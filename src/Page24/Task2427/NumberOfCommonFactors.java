67c7db19a636a3ad3edc84282011f91111f95bc0
molniyar <molniyar11@gmail.com>
2024-07-16 12:10:47 +0400

    2427 Number of common factors simplified code

--- src/Page24/Task2427/NumberOfCommonFactors.java
+++ src/Page24/Task2427/NumberOfCommonFactors.java
@@ -15,7 +15,7 @@ public class NumberOfCommonFactors {
                 break;
             }
         }
-        int result = 1;
+        int result = 0;
         for (int i = 1;i <= endNum;i++){
             if (a%i == 0 && b%i == 0){result++;}
         }
