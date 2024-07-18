fbcc371e8a5a9803ec5be9f400ee182efd16ca31
molniyar <molniyar11@gmail.com>
2024-07-18 20:37:18 +0400

    2956 Find common elements between two arrays simplified code

--- src/Page29/Task2956/FindCommonElementsBetweenTwoArrays.java
+++ src/Page29/Task2956/FindCommonElementsBetweenTwoArrays.java
@@ -1,31 +1,38 @@
 package Page29.Task2956;
 
+import java.util.BitSet;
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindCommonElementsBetweenTwoArrays {
     public static void main(String[] args) {
 
     }
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        int[] result = new int[2];
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
 
-        for (int i = 0; i < nums1.length; i++){
-            int keep = nums1[i];
-            for (int j = 0; j < nums2.length; j++){
-                if (keep == nums2[j]){
-                    result[0]++;
-                    break;
-                }
-            }
-        }
-        for (int i = 0; i < nums2.length; i++){
-            int keep = nums2[i];
-            for (int j = 0; j < nums1.length; j++){
-                if (keep == nums1[j]){
-                    result[1]++;
-                    break;
-                }
-            }
+        for (int j : nums1) {
+            set1.add(j);
         }
 
-        return result;
+        for (int j : nums2) {
+            set2.add(j);
+        }
+
+        int count1 = 0;
+        int count2 = 0;
+        for (int j : nums1) {
+            if (set2.contains(j)) {
+                count1++;
+            }
+        }
+        for (int j : nums2) {
+            if (set1.contains(j)) {
+                count2++;
+            }
+        }
+
+        return new int[]{count1, count2};
     }
 }
