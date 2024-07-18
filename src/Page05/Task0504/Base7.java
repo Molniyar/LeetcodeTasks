f89383ca88804a4bb893cc03fe508377e0bcef20
molniyar <molniyar11@gmail.com>
2024-07-18 20:36:32 +0400

    504 Base 7 simplified code

--- src/Page05/Task0504/Base7.java
+++ src/Page05/Task0504/Base7.java
@@ -2,9 +2,19 @@ package Page05.Task0504;
 
 public class Base7 {
     public static void main(String[] args) {
-
+        System.out.println(new Base7().convertToBase7(14));
     }
     public String convertToBase7(int num) {
-        return Integer.toString(num,7);
+        int res = 0;
+
+        int multiple = 1;
+
+        for (int i = num; i != 0; i /= 7){
+            res += (i % 7) * multiple;
+
+            multiple *= 10;
+        }
+
+        return res + "";
     }
 }
