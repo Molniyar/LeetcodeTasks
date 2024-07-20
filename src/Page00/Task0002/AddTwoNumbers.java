d03e3e2f289be6fde19af7b0416eaa4c1d195063
molniyar <molniyar11@gmail.com>
2024-07-20 10:20:20 +0400

    2 Add two numbers

---
+++ src/Page00/Task0002/AddTwoNumbers.java
@@ -0,0 +1,33 @@
+package Page00.Task0002;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            tail.next = new ListNode(digit);
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        ListNode result = dummyHead.next;
+        dummyHead.next = null;
+        return result;
+
+    }
+
+}
---
+++ src/Page00/Task0002/ListNode.java
@@ -0,0 +1,9 @@
+package Page00.Task0002;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
