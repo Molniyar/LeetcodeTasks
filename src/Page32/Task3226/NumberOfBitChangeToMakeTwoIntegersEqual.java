435527d80eb398d4b2d413b21e5ec4f1d917f8a5
molniyar <molniyar11@gmail.com>
2024-07-24 17:27:15 +0400

    3226 Number of bit change to make two integers equal

---
+++ src/Page32/Task3226/NumberOfBitChangeToMakeTwoIntegersEqual.java
@@ -0,0 +1,14 @@
+package Page32.Task3226;
+
+import java.lang.foreign.MemorySegment;
+import java.util.Base64;
+
+public class NumberOfBitChangeToMakeTwoIntegersEqual {
+    public static void main(String[] args) {
+
+    }
+    public int minChanges(int n, int k) {
+        if ( (n & k) != k) {return -1;}
+        return Integer.bitCount(n) - Integer.bitCount(k);
+    }
+}
