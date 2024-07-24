cad0e957fcff4fb3a994d77bce7e5a025897e7e6
molniyar <molniyar11@gmail.com>
2024-07-24 17:28:38 +0400

    2215 Find difference of tho arrays simplified code

--- src/Page22/Task2215/FindDiffirenceOfThoArrays.java
+++ src/Page22/Task2215/FindDiffirenceOfThoArrays.java
@@ -1,6 +1,8 @@
 package Page22.Task2215;
 
+import java.lang.annotation.Target;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 
 public class FindDiffirenceOfThoArrays {
@@ -9,37 +11,33 @@ public class FindDiffirenceOfThoArrays {
     }
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         List<List<Integer>> result = new ArrayList<>(2);
+
         result.add(new ArrayList<>());
         result.add(new ArrayList<>());
+
+        BitSet set1 = new BitSet(2001);
+        for (int i = 0; i < nums1.length; i++) {
+            set1.set(nums1[i]+1000);
+        }
+
+        BitSet set2 = new BitSet(2001);
+        for (int i = 0; i < nums2.length; i++){
+            set2.set(nums2[i]+1000);
+        }
 
         for (int i = 0;i < nums1.length; i++){
             int keep = nums1[i];
-            boolean shouldBeAdded = true;
 
-            for (int j = 0;j < nums2.length; j++){
-                if (keep == nums2[j]){
-                    shouldBeAdded = false;
-                    break;
-                }
-            }
-
-            if (shouldBeAdded && !result.get(0).contains(keep)){
+            if (!set2.get(keep+1000) && !result.get(0).contains(keep)){
                 result.get(0).add(keep);
             }
         }
 
         for (int i = 0;i < nums2.length; i++){
             int keep = nums2[i];
-            boolean shouldBeAdded = true;
 
-            for (int j = 0;j < nums1.length; j++){
-                if (keep == nums1[j]){
-                    shouldBeAdded = false;
-                    break;
-                }
-            }
 
-            if (shouldBeAdded && !result.get(1).contains(keep)){
+            if (!set1.get(keep + 1000) && !result.get(1).contains(keep)){
                 result.get(1).add(keep);
             }
         }
