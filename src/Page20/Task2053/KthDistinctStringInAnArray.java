bd645017776d7a3affea0730b299e081ca62cbc3
molniyar <molniyar11@gmail.com>
2024-08-05 16:21:31 +0400

    2053 Kth distinct string in an array

---
+++ src/Page20/Task2053/KthDistinctStringInAnArray.java
@@ -0,0 +1,26 @@
+package Page20.Task2053;
+
+import java.util.HashMap;
+
+public class KthDistinctStringInAnArray {
+    public static void main(String[] args) {
+
+    }
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> counter = new HashMap<>();
+
+        for (String v : arr) {
+            counter.put(v, counter.getOrDefault(v, 0) + 1);
+        }
+        for (String v : arr) {
+            if (counter.get(v) == 1) {
+                k--;
+                if (k == 0) {
+                    return v;
+                }
+            }
+        }
+        return "";
+        
+    }
+}
