4bf92efde14c11d1c58d4eef089a2e0339814696
molniyar <molniyar11@gmail.com>
2024-08-10 19:12:25 +0400

    101 Symmetric tree

---
+++ src/Page01/Task0101/SymmetricTree.java
@@ -0,0 +1,19 @@
+package Page01.Task0101;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null){return true;}
+
+        return isMirrored(root.left, root.right);
+    }
+
+    public boolean isMirrored(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {return true; }
+        if (left == null || right == null) {return false;}
+
+        return left.val == right.val && isMirrored(left.left, right.right) && isMirrored(left.right, right.left);
+    }
+}
