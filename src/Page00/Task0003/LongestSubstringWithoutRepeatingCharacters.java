1511870c9d4a47e0688c543aaf969a35b38e2f83
molniyar <molniyar11@gmail.com>
2024-08-11 12:08:24 +0400

    3 Longest substring without repeating characters

---
+++ src/Page00/Task0003/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,41 @@
+package Page00.Task0003;
+
+import java.util.BitSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+
+    }
+    public int lengthOfLongestSubstring(String s) {
+        BitSet set = new BitSet();
+
+        int res = 0;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            int n = s.charAt(i);
+
+            if (set.get(n)) {
+                if (res < count){
+                    res = count;
+                }
+
+                i -= count;
+                count = 0;
+
+                set = new BitSet();
+
+                continue;
+            }
+
+            set.set(n);
+            count++;
+        }
+
+        if (res < count){
+            res = count;
+        }
+
+        return res;
+    }
+}
