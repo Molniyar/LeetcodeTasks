0558621de3eab1026a3221ceec7cdc862300580b
Molniyar <molniyar11@gmail.com>
2024-10-28 17:24:50 +0400

    1380 Lucky numbers in a matrix  made myself

--- src/Page13/Task1380/LuckyNumbersInAMatrix.java
+++ src/Page13/Task1380/LuckyNumbersInAMatrix.java
@@ -1,38 +1,43 @@
 package Page13.Task1380;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class LuckyNumbersInAMatrix {
     public static void main(String[] args) {
-
+        System.out.println(new LuckyNumbersInAMatrix().luckyNumbers(new int[][]{{3,7,8},{9,11,13},{15,16,17}}));
     }
     public List<Integer> luckyNumbers (int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        List<Integer> inRow = new ArrayList<>(matrix.length);
+        int[] inCol = new int[matrix[0].length];
 
-        int[] row_minimums = new int[rows];
-        Arrays.fill(row_minimums, Integer.MAX_VALUE);
-        int[] col_maximums = new int[cols];
+        List<Integer> result = new ArrayList<>();
 
-        for (int row_ind = 0; row_ind < rows; row_ind++) {
-            for (int col_ind = 0; col_ind < cols; col_ind++) {
-                int el = matrix[row_ind][col_ind];
-                row_minimums[row_ind] = Math.min(row_minimums[row_ind], el);
-                col_maximums[col_ind] = Math.max(col_maximums[col_ind], el);
-            }
-        }
-
-        for (int row_ind = 0; row_ind < rows; row_ind++) {
-            for (int col_ind = 0; col_ind < cols; col_ind++) {
-                int el = matrix[row_ind][col_ind];
-                if (el == row_minimums[row_ind] && el == col_maximums[col_ind]) {
-                    return Collections.singletonList(el);
+        for (int i = 0; i < matrix.length; i++){
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < matrix[0].length; j++){
+                if (matrix[i][j] < min){
+                    min = matrix[i][j];
                 }
             }
+            inRow.add(min);
         }
-
-        return Collections.emptyList();
+        for (int i = 0; i < matrix[0].length; i++){
+            int max = 0;
+            for (int j = 0; j < matrix.length; j++){
+                if (matrix[j][i] > max){
+                    max = matrix[j][i];
+                }
+            }
+            inCol[i] = max;
+        }
+        for (int i = 0; i < inCol.length; i++){
+            if (inRow.contains(inCol[i])){
+                result.add(inCol[i]);
+            }
+        }
+        return result;
     }
 }
