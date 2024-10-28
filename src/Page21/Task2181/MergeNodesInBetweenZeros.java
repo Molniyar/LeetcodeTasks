546b9c2d057c655710dd0ceadb2754f6c8140ac9
Molniyar <molniyar11@gmail.com>
2024-10-28 17:25:49 +0400

    2181 Merge nodes in between zeros  made myself

--- src/Page21/Task2181/MergeNodesInBetweenZeros.java
+++ src/Page21/Task2181/MergeNodesInBetweenZeros.java
@@ -1,23 +1,41 @@
 package Page21.Task2181;
 
+import Page01.Task0169.MajorityElement;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeNodesInBetweenZeros {
     public static void main(String[] args) {
+        ListNode ex0 = new ListNode(0,new ListNode(2,new ListNode(0,new ListNode(5,new ListNode(2,new ListNode(0))))));
+        ListNode ex1 = new ListNode(0,new ListNode(1,new ListNode(0)));
+        ListNode ex2 = new ListNode(9,new ListNode(9,new ListNode(0,new ListNode(1))));
+        ListNode ex3 = new ListNode(1);
+
+        printListNode(new MergeNodesInBetweenZeros().mergeNodes(ex1));
 
     }
     public ListNode mergeNodes(ListNode head) {
-        ListNode prev=head.next, curr=head.next;
-        int sum=0;
-        for (; curr!=null; curr=curr.next){
-            int x=curr.val;
-            if (x!=0) sum+=x;
-            else{
-                prev.val=sum;
-                prev.next=curr.next;
-                prev=prev.next;
-                sum=0;
+        ListNode curr = head.next;
+        ListNode write = head;
+        for (int sum = 0;curr != null;curr = curr.next){
+            if (curr.val == 0){
+                write = write.next;
+                write.val = sum;
+                sum = 0;
+                continue;
             }
+            sum += curr.val;
+
         }
+        write.next = null;
         return head.next;
     }
 
+    public static void printListNode(ListNode head){
+        for (;head != null; head = head.next){
+            System.out.print(head.val+"-> ");
+        }
+    }
 }
