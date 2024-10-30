2890f2fe9c49a656c5a7d8156b64254f88d5f2f9
Molniyar <molniyar11@gmail.com>
2024-10-30 15:57:29 +0400

    68 Text justification  made myself

--- src/Page00/Task0068/TextJustification.java
+++ src/Page00/Task0068/TextJustification.java
@@ -1,5 +1,8 @@
 package Page00.Task0068;
 
+import com.sun.tools.javac.Main;
+
+import java.lang.annotation.Native;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,35 +11,33 @@ public class TextJustification {
 
     }
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> res = new ArrayList<>();
+        List<String> res  = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
 
-        List<String> current = new ArrayList<>();
-        int numOfLetters = 0;
+        int numOfChars = 0;
 
         for (String word : words) {
-            if (word.length() + current.size() + numOfLetters > maxWidth) {
-
-                for (int i = 0; i < maxWidth - numOfLetters; i++) {
-                    current.set(i % (current.size() - 1 > 0 ? current.size() - 1 : 1), current.get(i % (current.size() - 1 > 0 ? current.size() - 1 : 1)) + " ");
+            if (word.length() + curr.size() + numOfChars > maxWidth){
+                for (int i = 0; i < maxWidth - numOfChars; i++) {
+                    curr.set(i % (curr.size() - 1 > 0 ? curr.size() - 1 : 1), curr.get(i % (curr.size() - 1 > 0 ? curr.size() - 1 : 1)) + " ");
                 }
 
-                StringBuilder sb = new StringBuilder();
+                StringBuilder write = new StringBuilder();// writing the line to res
+                for (String s : curr) {write.append(s);}
+                res.add(write.toString());
+                curr.clear();
 
-                for (String s : current) {sb.append(s);}
-                res.add(sb.toString());
-                current.clear();
-                numOfLetters = 0;
+                numOfChars = 0;
             }
-            current.add(word);
-            numOfLetters += word.length();
+            curr.add(word);
+            numOfChars += word.length();
         }
-
         StringBuilder lastLine = new StringBuilder();
-        for (int i = 0; i < current.size(); i++) {
-            lastLine.append(current.get(i));
-            if (i != current.size() - 1) lastLine.append(" ");
+        for (int i = 0; i < curr.size(); i++){
+            lastLine.append(curr.get(i));
+            if (i != curr.size()-1) {lastLine.append(" ");}
         }
-        while (lastLine.length() < maxWidth) {lastLine.append(" ");}
+        while (lastLine.length() < maxWidth){lastLine.append(" ");}
         res.add(lastLine.toString());
 
         return res;
