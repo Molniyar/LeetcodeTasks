ac5a593c390efb4c46f76f8b27f75912d58567c8
Molniyar <molniyar11@gmail.com>
2024-11-01 15:57:26 +0400

    1704  determine if string halves are alike  simplified code

--- src/Page17/Task1704/DetermineIfStringHalvesAreAlike.java
+++ src/Page17/Task1704/DetermineIfStringHalvesAreAlike.java
@@ -1,21 +1,29 @@
 package Page17.Task1704;
 
+import java.util.BitSet;
+
 public class DetermineIfStringHalvesAreAlike {
     public static void main(String[] args) {
-
+        System.out.println(new DetermineIfStringHalvesAreAlike().halvesAreAlike("textbook"));
     }
     public boolean halvesAreAlike(String s) {
+        s = s.toLowerCase();
+
+        BitSet set = new BitSet(26);
+        set.set('a'-'a');
+        set.set('e'-'a');
+        set.set('i'-'a');
+        set.set('o'-'a');
+        set.set('u'-'a');
+
         int c1 = 0;
         int c2 = 0;
 
-        for (int i = 0, j = s.length()/2;j < s.length();i++, j++){
-            switch (s.charAt(i)){
-                case 'a','e','i','o','u','A','E','I','O','U' -> c1++;
-            }
-            switch (s.charAt(j)){
-                case 'a','e','i','o','u','A','E','I','O','U' -> c2++;
-            }
+        for (int i = 0, j = s.length()/2; j < s.length(); i++, j++){
+            if (set.get(s.charAt(i)-'a')){c1++;}
+            if (set.get(s.charAt(j)-'a')){c2++;}
         }
+
         return c1 == c2;
     }
 }
