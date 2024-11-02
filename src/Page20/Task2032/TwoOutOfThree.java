c277222c642fde487facea90d43187bf1c26a5fc
Molniyar <molniyar11@gmail.com>
2024-11-02 13:49:28 +0400

    2032 Two out of three  simplified code

--- src/Page20/Task2032/TwoOutOfThree.java
+++ src/Page20/Task2032/TwoOutOfThree.java
@@ -11,24 +11,19 @@ public class TwoOutOfThree {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         List<Integer> res = new ArrayList<>();
 
-        BitSet set1 = new BitSet(101);
-        for (int i = 0; i < nums1.length; i++){set1.set(nums1[i]);}
-        BitSet set2 = new BitSet(101);
-        for (int i = 0; i < nums2.length; i++){set2.set(nums2[i]);}
-        BitSet set3 = new BitSet(101);
-        for (int i = 0; i < nums3.length; i++){set3.set(nums3[i]);}
+        BitSet set = new BitSet(101);
+        for (int i = 0; i < nums1.length; i++){set.set(nums1[i]);}
 
+        for (int n : nums2){//                    \
+            if (set.get(n)){//                     \
+                res.add(n);//                       \
+                set.set(n,false);//                  }- nums2
+            }//                                     /
+        }//                                        /
+        for (int n : nums2){set.set(n,true);}//   /
 
-        for (int i = 1; i < 101; i++){
-            int c = 0;
-
-            if (set1.get(i)){c++;}
-            if (set2.get(i)){c++;}
-            if (set3.get(i)){c++;}
-
-            if (c > 1){
-                res.add(i);
-            }
+        for (int n : nums3){
+            if (set.get(n) && !res.contains(n)){res.add(n);}
         }
 
         return res;
