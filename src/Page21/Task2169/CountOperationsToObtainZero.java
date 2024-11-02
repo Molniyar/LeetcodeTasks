48f6e2b3a235e530694293dd8675e2b2d39a3031
Molniyar <molniyar11@gmail.com>
2024-11-02 13:49:54 +0400

    2169 Count operations to obtain zero  simplified code

--- src/Page21/Task2169/CountOperationsToObtainZero.java
+++ src/Page21/Task2169/CountOperationsToObtainZero.java
@@ -5,16 +5,13 @@ public class CountOperationsToObtainZero {
 
     }
     public int countOperations(int num1, int num2) {
-        if (num1 == 0 || num2 == 0){
-            return 0;
-        }
-        else {
-            if (num1 > num2){
-                num1 -= num2;
-            }
-            else num2 -= num1;
+        int res = 0;
 
-            return countOperations(num1,num2)+1;
+        for (;num1 != 0 && num2 != 0;res++) {
+            if (num1 > num2){num1 -= num2;}
+            else num2 -= num1;
         }
+
+        return res;
     }
 }
