79ab4b7ef8c512a6d7eec76225f546a099b34602
Molniyar <molniyar11@gmail.com>
2024-11-03 15:35:37 +0400

    1637 Widest vertical area between two points containing no points  simplified code

--- src/Page16/Task1637/WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
+++ src/Page16/Task1637/WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
@@ -2,25 +2,21 @@ package Page16.Task1637;
 
 import java.util.Arrays;
 import java.util.BitSet;
+import java.util.Comparator;
 import java.util.HashSet;
 
 public class WidestVerticalAreaBetweenTwoPointsContainingNoPoints {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {//������� ��� ���� ����
+        System.out.println(new WidestVerticalAreaBetweenTwoPointsContainingNoPoints().maxWidthOfVerticalArea(new int[][] {{8,7},{9,9},{7,4},{9,7}}));
     }
     public int maxWidthOfVerticalArea(int[][] points) {
-        int[] sorted = new int[points.length];
-
-        for (int i = 0; i < points.length; i++){
-            sorted[i] = points[i][0];
-        }
-
-        Arrays.sort(sorted);
+        Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
 
         int res  = 0;
-        int prev = sorted[0];
+        int prev = points[0][0];
 
-        for (int num : sorted){
+        for (int i = 0; i < points.length; i++){
+            int num = points[i][0];
             int distance = num-prev;
 
             if (res < distance){
