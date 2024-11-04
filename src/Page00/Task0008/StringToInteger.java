3a6ba81519e0a7cc41e3b2604ef3607875cc157b
Molniyar <molniyar11@gmail.com>
2024-11-04 19:45:12 +0400

    8 String to integer  simplified code

--- src/Page00/Task0008/StringToInteger.java
+++ src/Page00/Task0008/StringToInteger.java
@@ -4,34 +4,53 @@ import java.io.FileNotFoundException;
 
 public class StringToInteger {
     public static void main(String[] args) {
-        System.out.println(new StringToInteger().myAtoi("-91283472332"));
+        System.out.println(new StringToInteger().myAtoi("  -0012a42"));
     }
     public int myAtoi(String s) {
         int startIndex = 0;
-
         boolean hasMinus = false;
+        boolean isNum    = false;
+        int res = 0;
 
         while (startIndex < s.length()) {
             boolean break_ = false;
             switch (s.charAt(startIndex)){
                 case '-' -> {
+                    if (isNum){break_ = true;break;}
                     hasMinus=true;
-                    startIndex++;
-                    break_ = true;
+                    isNum = true;
+                    //System.out.println("minus");
                 }
                 case '+' -> {
+                    if (isNum){break_ = true;break;}
                     hasMinus=false;
-                    startIndex++;
-                    break_ = true;
+                    isNum = true;
+                    //System.out.println("plus");
                 }
                 case '0','1','2','3','4','5','6','7','8','9' -> {
-                    break_ = true;
+                    try {
+                        res *= 10;
+                        res += s.charAt(startIndex)-'0';
+
+                        if (res%10 != (s.charAt(startIndex)-'0') || res < 0){
+                            if (hasMinus){return -2147483648;}
+                            return 2147483647;
+                        }
+                    } catch (NumberFormatException e) {
+
+                        if (hasMinus){return -2147483648;}
+                        return 2147483647;
+                    }
+                    //System.out.println("res is: " + res + " now");
+                    isNum = true;
                 }
                 case ' ' -> {
-                    break;
+                    if (isNum){break_ = true;break;}
+                    //System.out.println("   _   ");
                 }
                 default -> {
-                    return 0;
+                    //System.out.println("default");
+                    break_ = true;
                 }
             }
             if (break_){
@@ -40,26 +59,8 @@ public class StringToInteger {
             startIndex++;
         }
 
-
-        int res = 0;
-
-        for (;startIndex < s.length() && s.charAt(startIndex) >= '0' && s.charAt(startIndex) <= '9';startIndex++){
-            try {
-                res *= 10;
-                res += s.charAt(startIndex)-'0';
-
-                if (res%10 != (s.charAt(startIndex)-'0') || res < 0){
-                    if (hasMinus){return -2147483648;}
-                    return 2147483647;
-                }
-            } catch (NumberFormatException e) {
-
-                if (hasMinus){return -2147483648;}
-                return 2147483647;
-            }
-        }
-
         if (hasMinus){res = -res;}
         return res;
     }
+
 }
